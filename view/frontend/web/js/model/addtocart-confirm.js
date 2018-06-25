afaa6e6b9d05096013a2439ff2cc58ba9bb2ec44
denistrator <denistrator@yandex.ua>
2018-06-25 22:20:42 +0300

    reformat/clean up module

--- view/frontend/web/js/model/addtocart-confirm.js
+++ view/frontend/web/js/model/addtocart-confirm.js
@@ -1,55 +1,54 @@
 /**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright © 2018 Studio Raz. All rights reserved.
+ * For more information contact us at dev@studioraz.co.il
  */
+
 /*jshint browser:true jquery:true*/
 /*global alert*/
-define(
-    [
-        'jquery',
-        'Magento_Ui/js/modal/modal',
-        'mage/translate'
-    ],
-    function ($, modal, $t) {
-        'use strict';
 
-        return {
-            modalWindow: null,
+define([
+    'jquery',
+    'Magento_Ui/js/modal/modal',
+    'mage/translate'
+], function ($, modal, $t) {
+    'use strict';
 
-            /** Create popUp window for provided element */
-            createPopUp: function (element) {
-                this.modalWindow = element;
-                var options = {
-                    'autoOpen': false,
-                    'type': 'popup',
-                    'clickableOverlay' : true,
-                    'modalClass': 'popup-addtocart-confirm',
-                    'responsive': true,
-                    'innerScroll': true,
-                    'buttons': [
-                        {
-                            text: $t('Continue Shopping'),
-                            class: 'action secondary action-continue-shopping',
-                            click: function () {
-                                this.closeModal();
-                            }
-                        },
-                        {
-                            text: $t('Go to Cart Page'),
-                            class: 'action primary action-go-to-cart',
-                            click: function () {
-                                window.location.href = window.addtocartConfirm.shoppingCartUrl;
-                            }
+    return {
+        modalWindow: null,
+
+        /** Create popUp window for provided element */
+        createPopUp: function (element) {
+            this.modalWindow = element;
+            var options = {
+                'autoOpen': false,
+                'type': 'popup',
+                'clickableOverlay': true,
+                'modalClass': 'popup-addtocart-confirm',
+                'responsive': true,
+                'innerScroll': true,
+                'buttons': [
+                    {
+                        text: $t('Continue Shopping'),
+                        class: 'action secondary action-continue-shopping',
+                        click: function () {
+                            this.closeModal();
                         }
-                    ]
-                };
-                modal(options, $(this.modalWindow));
-            },
+                    },
+                    {
+                        text: $t('Go to Cart Page'),
+                        class: 'action primary action-go-to-cart',
+                        click: function () {
+                            window.location.href = window.addtocartConfirm.shoppingCartUrl;
+                        }
+                    }
+                ]
+            };
+            modal(options, $(this.modalWindow));
+        },
 
-            /** Show login popup window */
-            showModal: function () {
-                $(this.modalWindow).modal('openModal');
-            }
+        /** Show login popup window */
+        showModal: function () {
+            $(this.modalWindow).modal('openModal');
         }
     }
-);
+});
